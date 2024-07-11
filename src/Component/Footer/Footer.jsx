7b74d6f00437bb7969d8ff266ee5d2794891fb3e
Chaiwit-Ch <chaiwit.ch@outlook.com>
2024-07-11 11:25:49 +0700

    edit footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer_con}>
         Copyright © 2024. All Rights Reserved.
     </footer>
   )
